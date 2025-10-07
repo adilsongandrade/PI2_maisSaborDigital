deb7743ec9e79806d4871446d52496d2251d522b
Adilson G Andrade <adilsongandrade@gmail.com>
2025-10-07 11:15:00 -0300

    update permission build.sh

--- build.sh
+++ build.sh
@@ -3,5 +3,5 @@
 # Este script será executado pelo Vercel
 echo "BUILD START"
 pip install -r requirements.txt
-py manage.py collectstatic --no-input --clear # Ação crucial
+python3 manage.py collectstatic --no-input --clear # Ação crucial
 echo "BUILD END"
